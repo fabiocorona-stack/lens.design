0d6e605dc2459fb7983d3c91c2e7c8bc4ea3fc57
Fabio Corona <fabiocorona@gmail.com>
2026-03-28 16:47:47 +0100

    Update scan.js

--- api/scan.js
+++ api/scan.js
@@ -1,4 +1,4 @@
-export const config = { api: { bodyParser: { sizeLimit: '10mb' }, responseLimit: '10mb' } };
+export const config = { api: { bodyParser: { sizeLimit: '4mb' } } };
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
